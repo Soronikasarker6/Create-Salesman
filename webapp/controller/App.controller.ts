f36e2ccd2fb443cdcbef3e4d5d6658f9554c438b
soronikasarkerpinkey <spinkey@schertech.com>
2023-11-20 17:59:56 +0600

    TableSelectDialog

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,4 +1,9 @@
+import Dialog from "sap/m/Dialog";
+import Control from "sap/ui/core/Control";
+import UI5Element from "sap/ui/core/Element";
+import Fragment from "sap/ui/core/Fragment";
 import Controller from "sap/ui/core/mvc/Controller";
+import Context from "sap/ui/model/odata/v4/Context";
 
 
 /**
@@ -6,7 +11,28 @@ import Controller from "sap/ui/core/mvc/Controller";
  */
 export default class App extends Controller {
 
-
+  dataDialog: Promise<Control | Control[]>;
+  rowContext: Context;
   public onInit(): void {}
 
+  public onValueHelpRequest(oEvent: any): void{
+    var sInputValue = oEvent.getSource().getValue(),
+    oView = this.getView();
+
+  if (!this.dataDialog) {
+    this.dataDialog = Fragment.load({
+      id: oView.getId(),
+      name: "create.salesman.view.fragment.supplier",
+      controller: this
+    }).then(function (dialog: Control | Control[]) {
+      oView.addDependent(dialog as UI5Element);
+      return dialog;
+    });
+  }
+  this.dataDialog.then(function(dialog: Control | Control[]) {
+
+    (dialog as Dialog).open();
+  });
+
+  }
 }
