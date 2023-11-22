4994d83d34a8f1ce0195e9ed53f683b56df38038
soronikasarkerpinkey <spinkey@schertech.com>
2023-11-22 10:23:19 +0600

    evaluate json data

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -4,6 +4,8 @@ import UI5Element from "sap/ui/core/Element";
 import Fragment from "sap/ui/core/Fragment";
 import Controller from "sap/ui/core/mvc/Controller";
 import Context from "sap/ui/model/odata/v4/Context";
+import Component from "sap/ui/core/Component";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 
 /**
@@ -13,7 +15,32 @@ export default class App extends Controller {
 
   dataDialog: Promise<Control | Control[]>;
   rowContext: Context;
-  public onInit(): void {}
+  public onInit(): void {
+    (this.getOwnerComponent() as Component).setModel(
+      new JSONModel({
+        salesman: [
+          {
+            name: "Patric",
+            code: "395-9823",
+            region: "TN",
+            companyCode: "Z001",
+            start:"Oct 23, 2023",
+            end:"Oct 21, 2024"
+          },
+          {
+            name: "Alex",
+            code: "985-9823",
+            region: "TN",
+            companyCode: "Z001",
+            start:"Oct 3, 2023",
+            end:"Oct 7, 2024"
+          },
+        ],
+      }),
+      "salesman"
+    );
+
+  }
 
   public onValueHelpRequest(oEvent: any): void{
     var sInputValue = oEvent.getSource().getValue(),
@@ -34,5 +61,18 @@ export default class App extends Controller {
     (dialog as Dialog).open();
   });
 
+  }
+
+  public onValueHelpClose (oEvent: any): void{
+    var oBinding = oEvent.getSource().getBinding("items");
+    oBinding.filter([]);
+    let aContexts = oEvent.getParameter("selectedContexts");
+    if (aContexts && aContexts.length) {
+       aContexts.map(function (oContext:any){
+      let selected = oContext.getObject().name;
+      console.log(selected)
+      return selected;
+       })
+    }
   }
 }
